e16f8d6a2dce3f0bc2ddd0aa73c992fb211506a8
antrioe <antrioe@gmali.com>
2026-02-26 11:55:45 +0800

    ci(deps): ci

--- src/layouts/BaseLayout.vue
+++ src/layouts/BaseLayout.vue
@@ -22,7 +22,7 @@ const { collapsed, contentFullScreen } = storeToRefs(useAppStore())
       <NLayoutHeader position="absolute" class="z-999 w-full flex-col">
         <BaseLayoutHeader />
       </NLayoutHeader>
-      <NLayoutContent content-style="padding: 10px;" class="wh-full bg-black/1! dark:bg-black/80!" :class="contentFullScreen ? 'p-t-[40px] p-b-0' : 'p-t-[100px] p-b-[40px]'">
+      <NLayoutContent content-class="p-2 overflow-hidden" class="wh-full bg-black/1! dark:bg-black/80!" :class="contentFullScreen ? 'p-t-[40px] p-b-0' : 'p-t-[100px] p-b-[40px]'">
         <slot />
       </NLayoutContent>
       <NLayoutFooter v-if="!contentFullScreen" position="absolute" class="h-[40px] w-full">
--- src/layouts/BaseLayoutFooter.vue
+++ src/layouts/BaseLayoutFooter.vue
@@ -1,4 +1,5 @@
 <script setup lang='ts'>
+import { NTooltipButton } from '@oiij/naive-ui/components'
 import dayjs from 'dayjs'
 import { sha } from '~build/git'
 import now from '~build/time'
@@ -8,35 +9,37 @@ const { date, time } = useClock()
 const { browser, os } = useUaParser()
 
 const fps = useFps()
+
+const { isSupported: memoryIsSupported, memory } = useMemory()
+function sizeFormat(size?: number) {
+  return size ? (size / 1024 / 1024).toFixed(2) : 'N/A'
+}
 </script>
 
 <template>
   <div class="wh-full flex items-center justify-end gap-[10px] border-l bg-white p-x-[10px] dark:border-white/10 dark:bg-black">
-    <NTag :bordered="false" type="info">
+    <NTooltipButton
+      :tooltip="`Build Sha ${sha.slice(0, 7)}, At ${dayjs(now).format('YYYY-MM-DD HH:mm:ss')}.`"
+      :button-props="{ size: 'small', quaternary: true, type: 'info' }"
+    >
       <template #icon>
         <i class="i-mage-check-square text-[14px]" />
       </template>
-      {{ `Build Sha ${sha.slice(0, 7)}, At ${dayjs(now).format('YYYY-MM-DD HH:mm:ss')}.` }}
-    </NTag>
-    <NTag :bordered="false">
+    </NTooltipButton>
+    <NTooltipButton
+      :tooltip="`${browser.name}${os.name}, ${memoryIsSupported ? `${sizeFormat(memory?.usedJSHeapSize)}/${sizeFormat(memory?.jsHeapSizeLimit)}MB` : `N/A`}, ${fps}FPS`"
+      :button-props="{ size: 'small', quaternary: true, type: 'default' }"
+    >
       <template #icon>
-        <i class="i-mage-chip text-[14px]" />
+        <i class="i-mage-information-circle text-[14px]" />
       </template>
-      {{ browser.name }}
-      {{ os.name }}
-    </NTag>
-    <NTag :bordered="false">
-      <template #icon>
-        <i class="i-mage-clock text-[14px]" />
-      </template>
-      {{ `${date} ${time} ` }}
-    </NTag>
-    <NTag :bordered="false">
-      <template #icon>
-        <span class="text-[14px]">Fps</span>
-      </template>
-      {{ fps }}
-    </NTag>
+    </NTooltipButton>
+    <NTooltipButton
+      :tooltip="`${date} ${time} `"
+      :button-props="{ size: 'small', quaternary: true, type: 'default' }"
+    >
+      {{ time }}
+    </NTooltipButton>
   </div>
 </template>
 
